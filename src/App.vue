61eb4180d53b1b0d513fedf1a12db4dc9a4c46d9
xiaocheng <707996290@qq.com>
2018-04-21 00:20:09 +0800

    reset and set sass-loader and node-sass

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang='scss'>
+@import './css/reset.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
